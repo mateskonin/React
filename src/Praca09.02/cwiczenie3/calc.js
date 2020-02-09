6855531642866f05357af36d50cc4a00f3dff4a0
mateskonin <dawidmatecki@poczta.onet.pl>
2020-02-09 13:24:16 +0100

    Praca Domowa 2,3

--- src/Praca09.02/cwiczenie3/calc.js
+++ src/Praca09.02/cwiczenie3/calc.js
@@ -1,14 +1,27 @@
 import React from "react";
 
 export default class Calc extends React.Component{
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            number: 0
+        };
+    }
+    showPrompt = () => {
+        var value = prompt("Podaj Liczbe");
+        let tempVal=this.state.number + parseInt(value);
+        this.setState({number: tempVal})
+    }
     render() {
 
-        var value = prompt("Podaj Liczbe")
+
 
         return (
             <div>
-
+                <button onClick={this.showPrompt}>Wprowadz liczbe</button>
+                <p>Wynik: {this.state.number}</p>
             </div>
-        )
+        );
     }
 }
--- src/Praca09.02/cwiczenie3/parent.js
+++ src/Praca09.02/cwiczenie3/parent.js
@@ -1,6 +1,14 @@
 import React from "react";
 
 export default class Parent extends React.Component{
+
+    constructor(props) {
+        super(props);
+        this.state ={
+            number: 0
+        }
+    }
+
     render() {
         return (
             <div>
