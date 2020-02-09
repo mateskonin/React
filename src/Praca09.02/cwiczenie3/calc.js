f5a4c72f61e585fed86aa4f39bf0702057172be4
mateskonin <dawidmatecki@poczta.onet.pl>
2020-02-09 12:53:05 +0100

    Praca Domowa 2,3

--- src/Praca09.02/cwiczenie3/calc.js
+++ src/Praca09.02/cwiczenie3/calc.js
@@ -2,6 +2,9 @@ import React from "react";
 
 export default class Calc extends React.Component{
     render() {
+
+        var value = prompt("Podaj Liczbe")
+
         return (
             <div>
 
